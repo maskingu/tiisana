29637fcddb22a90739a8912fa062cb98ab523c9b
maskingu <kengomei@outlook.com>
2020-10-23 20:37:07 +0900

    修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_rich_text :content
   has_one_attached :image
+  belongs_to :user
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :image, :content, presence: true
