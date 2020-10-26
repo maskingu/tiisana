9ff6c19c4c45499a52eb81a622003844b5558f3f
maskingu <kengomei@outlook.com>
2020-10-26 20:22:34 +0900

    ActiveTextの内容を検索

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,20 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :image, :content, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
+
+  def self.search(search)
+    if search != ""
+      scope :search, -> (search_param = nil) {
+        return if search_param.blank?
+        joins("INNER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = posts.id AND action_text_rich_texts.record_type = 'Post'")
+        .where("action_text_rich_texts.body LIKE ? OR posts.title LIKE ? ", "%#{search_param}%", "%#{search_param}%")
+      }
+  else
+    Post.all
+  end
+  end
 end
+
+
