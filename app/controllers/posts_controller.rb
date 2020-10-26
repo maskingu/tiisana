13d061b65752b135145674d6504b9c2a8b96152e
maskingu <kengomei@outlook.com>
2020-10-26 16:16:35 +0900

    基本的なコメント機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,8 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user)
   end
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_rich_text :content
   has_one_attached :image
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :image, :content, presence: true
