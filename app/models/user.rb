ed34c4a906bc8983643d0f890cb549606aacd1f3
maskingu <kengomei@outlook.com>
2020-10-28 20:14:06 +0900

    エラーメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable
 
         has_many :posts, dependent: :destroy
         has_many :comments, dependent: :destroy
@@ -16,9 +15,8 @@ class User < ApplicationRecord
           EMAIL_REGEX = /@+/.freeze
           validates :email, uniqueness: { case_sensitive: true }, format: { with: EMAIL_REGEX, message: '@を使用してください' }
           PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-          validates :password, confirmation: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両  方を含めて設定してください' }
+          validates :password, confirmation: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
           
         end
-
         
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it '必須項目が全てあれば登録できること' do
+      expect(@user).to be_valid
+    end
+
+    it 'nicknameが空では登録できないこと' do
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include('ニックネームを入力してください')
+    end
+
+    it 'emailが空では登録できないこと' do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Eメールを入力してください')
+    end
+
+    it '重複したemailが存在する場合登録できないこと' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
+    end
+
+    it 'アドレスの中に@が無いと登録できない' do
+      @user.email = 'hogehogehoge'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Eメール@を使用してください')
+    end
+
+    it 'passwordが空では登録できないこと' do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include('パスワードを入力してください')
+    end
+
+    it 'passwordが存在してもpassword_confirmationが空では登録できないこと' do
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include('パスワード（確認用）とパスワードの入力が一致しません')
+    end
+
+    it 'passwordが6文字以上であれば登録できること' do
+      @user.password = '123abc'
+      @user.password_confirmation = '123abc'
+      expect(@user).to be_valid
+    end
+
+    it 'passwordが5文字以下であれば登録できないこと' do
+      @user.password = '123ab'
+      @user.password_confirmation = '123ab'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
+    end
+
+    it 'passwordが半角数字のみなら登録できないこと' do
+      @user.password = '123456'
+      @user.password_confirmation = '123456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('パスワードには英字と数字の両方を含めて設定してください')
+    end
+
+    it 'passwordが半角英字のみなら登録できないこと' do
+      @user.password = 'abcdefg'
+      @user.password_confirmation = 'abcdefg'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('パスワードには英字と数字の両方を含めて設定してください')
+    end
+
+  end
 end
