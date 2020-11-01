d0a2b1bc33d49a6bb8237974009197e2ba785d54
maskingu <kengomei@outlook.com>
2020-11-01 13:20:34 +0900

    あ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
 require("trix")
 require("@rails/actiontext")
 require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// = require jquery3
+// = require popper
+// = require bootstrap-sprockets
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,6 +21,3 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-// = require jquery3
-// = require popper
-// = require bootstrap-sprockets
