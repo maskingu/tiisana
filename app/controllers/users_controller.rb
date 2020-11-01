6750de5fc44e8632eb6ad898390d8e92eb27753e
maskingu <kengomei@outlook.com>
2020-11-01 14:53:46 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @nickname = @user.nickname
-    @posts = @user.posts
+    @posts = @user.posts.page(params[:page]).per(10)
   end
   
 
   def following
     @user  = User.find(params[:id])
-    @users = @user.followings
+    @users = @user.followings.page(params[:page]).per(1)
     render 'show_follow'
 end
 
 def followers
   @user  = User.find(params[:id])
-  @users = @user.followers
+  @users = @user.followers.page(params[:page]).per(1)
   render 'show_follower'
 end
 
