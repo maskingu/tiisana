cff46b7be1eb6ed203233da0a2b9e6d1268af242
maskingu <kengomei@outlook.com>
2020-11-04 10:28:45 +0900

    削除機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   has_rich_text :content
   has_one_attached :image
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
 
